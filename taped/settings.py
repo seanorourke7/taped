80e070b01000f2ee37a1f62fd44d7bb357c4e4e6
seanorourke <s.orourke99@gmail.com>
2024-02-11 15:04:01 +0000

    debug switched to test in heroku

--- taped/settings.py
+++ taped/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
