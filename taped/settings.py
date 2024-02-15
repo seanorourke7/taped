179a4046afe79bd47eecc04ed02aa2f3f647416a
seanorourke <s.orourke99@gmail.com>
2024-02-15 19:59:31 +0000

    attempt to fix css issue in heroku

--- taped/settings.py
+++ taped/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','taped-8201a532e6b6.herokuapp.com','localhost','8000-seanorourke7-taped-8p3uosoo2l3.ws-eu108.gitpod.io']
 
