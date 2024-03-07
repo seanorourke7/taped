13d6ee3b40564333804ef0e12214017f645f20a3
sean orourke <s.orourke99@gmail.com>
2024-03-07 14:38:58 +0000

    update 404 link

--- taped/urls.py
+++ taped/urls.py
@@ -1,23 +1,9 @@
-"""luca_taped URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +16,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'taped.views.handler404'
