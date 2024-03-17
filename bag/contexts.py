bd8cb23c9b3da2b3ebe7e46b1d5572a2c25f1450
sean orourke <s.orourke99@gmail.com>
2024-03-17 20:44:35 +0000

    update contexts.py to calculate sale prices

--- bag/contexts.py
+++ bag/contexts.py
@@ -8,30 +8,52 @@ def bag_contents(request):
 
     bag_items = []
     total = 0
+    off_sale_total = 0
+    sale_total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
-            bag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
+            if product.on_sale:
+                sale_total += item_data * product.sale_price
+                product_count += item_data
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                })           
+            else:
+                off_sale_total += item_data * product.price
+                product_count += item_data
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                })           
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
-                product_count += quantity
-                bag_items.append({
-                    'item_id': item_id,
-                    'quantity': quantity,
-                    'product': product,
-                    'size': size,
-                })
+                if product.on_sale:
+                    sale_total += quantity * product.sale_price
+                    product_count += quantity
+                    bag_items.append({
+                        'item_id': item_id,
+                        'quantity': quantity,
+                        'product': product,
+                        'size': size,
+                    })
+                else:
+                    off_sale_total += quantity * product.price
+                    product_count += quantity
+                    bag_items.append({
+                        'item_id': item_id,
+                        'quantity': quantity,
+                        'product': product,
+                        'size': size,
+                    })
+        total = sale_total + off_sale_total        
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
