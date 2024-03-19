665709ece9f0033faffc6e57663cdacebce763db
sean orourke <s.orourke99@gmail.com>
2024-03-19 21:06:08 +0000

    debug false

--- taped/settings.py
+++ taped/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'taped-8201a532e6b6.herokuapp.com', 'localhost', '8000-seanorourke7-taped-xw2szzp3nmz.ws-eu110.gitpod.io']
 
